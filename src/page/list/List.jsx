6c67f2810bbda5e5b178c3fbc5ef46657dbfd1c2
yube01 <114811769+yube01@users.noreply.github.com>
2023-01-29 20:12:22 +0545

    final

--- src/page/list/List.jsx
+++ src/page/list/List.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import Sidebar from "../../components/sidebar/SideBar"
+import Navbar from "../../components/navbar/Navbar"
 import "./list.scss"
+import Datatable from "../../components/datatable/Datatable"
 const List = () => {
   return (
-    <div>
-
-      List
-      
+    <div className="list">
+      <Sidebar/>
+      <div className="listContainer">
+        <Navbar/>
+        datatable
+      </div>
     </div>
   )
 }
